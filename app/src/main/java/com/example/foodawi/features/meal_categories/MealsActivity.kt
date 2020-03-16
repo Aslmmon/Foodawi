189208fa79ab98f4e8cbad9a62234a7db5b320a1
Aslmmon <Aslmmon993@gmail.com>
2020-03-16 14:48:41 +0200

    * Added Meals api implementation

--- app/src/main/java/com/example/foodawi/features/meal_categories/MealsActivity.kt
+++ app/src/main/java/com/example/foodawi/features/meal_categories/MealsActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.example.foodawi.R
 import com.example.foodawi.common.Constants
 import com.example.foodawi.common.model.categories.mealsResponse.Meal
 import com.example.foodawi.features.home_categories.adapter.CategoriesAdapter
 import com.example.foodawi.features.meal_categories.adapter.MealsAdapter
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_details.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -26,7 +28,6 @@ class MealsActivity : AppCompatActivity(R.layout.activity_details), MealsAdapter
             dataRecieved?.let {
                 Log.i(javaClass.simpleName, it)
                 mealViewModel.getMealsOfCategories(it)
-
             }
         }
 
